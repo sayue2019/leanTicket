b92300f0cd7ef510d49cf5c35f77a577599de4c8
willgeek <willgeek@qq.com>
2019-11-05 08:33:24 +0800

    偿试修复错误:The user cannot be altered by a client without the session

--- api/oauth.js
+++ api/oauth.js
@@ -33,10 +33,10 @@ exports.loginCallback = (callbackUrl) => {
   return (req, res) => {
     wechat.getAccessToken(req.query.code, callbackUrl).then((accessToken) => {
       accessToken.openid = '' + accessToken.UserId
-      console.log(JSON.stringify(accessToken))
+      //console.log(JSON.stringify(accessToken))
       return AV.User.loginWithAuthData(accessToken, 'wechat')
     }).then((user) => {
-      console.log(JSON.stringify(user))
+      //console.log(JSON.stringify(user))
       if (_.isEqual(user.createdAt, user.updatedAt)) {
         // 第一次登录，从 LeanCloud 初始化用户信息
         return initUserInfo(user)
@@ -51,6 +51,7 @@ exports.loginCallback = (callbackUrl) => {
 const initUserInfo = (user) => {
   return getClientInfo(user)
   .then((client) => {
+    console.log(JSON.stringify(client))
     return user.save({
       username: client.userid.toLowerCase(),
       name: client.name,
